45996e60774b7f0755a35b8093e02f9d26b723cc
zalevskiy <zaleva100@gmail.com>
2025-11-11 22:22:09 +0200

    add hw js 9

---
+++ src/js/2-form.js
@@ -0,0 +1,26 @@
+const form = document.querySelector('.feedback-form');
+const STORAGE_KEY = 'feedback-form-state';
+
+const savedData = JSON.parse(localStorage.getItem(STORAGE_KEY)) || {};
+form.email.value = savedData.email || '';
+form.message.value = savedData.message || '';
+
+form.addEventListener('input', () => {
+  const formData = {
+    email: form.email.value,
+    message: form.message.value,
+  };
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+});
+
+form.addEventListener('submit', evt => {
+  evt.preventDefault();
+
+  console.log('Form submitted:', {
+    email: form.email.value,
+    message: form.message.value,
+  });
+
+  localStorage.removeItem(STORAGE_KEY);
+  form.reset();
+});
